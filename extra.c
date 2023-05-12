f6289bcffc50e6adfa3b20c979e4f62dbe224f95
hanyoq15 <130302221+hanyoq15@users.noreply.github.com>
2023-05-12 23:43:03 +0900

    Update extra.c

--- extra.c
+++ extra.c
@@ -1,53 +1,4 @@
-int addRecruit(Recruit *s){
-  printf("\n출발지 입력 ");
-  scanf(" %[^\n]s", s->depart);
-  printf("도착지 입력 ");
-  scanf(" %[^\n]s", s->arrive );
-  printf("출발날짜 (예, 0326) ");
-  scanf("%hd", &s->date);
-  printf("출발시간 (예, 1940) ");
-  scanf("%hd", &s->time);
-  printf("모집인원 (2~4) ");
-  scanf("%hd", &s->num);
-  while(s->num < 2 || s->num > 4){
-    printf("모집 인원은 2명에서 4명까지 가능합니다.\n");
-    printf("모집인원 (2~4) ");
-    scanf("%hd", &s->num);
-  }
-  printf("모집종류 (택시 : T, 카풀 : C) ");
-  scanf(" %c", &s->type);
-  s->now_num = 1;
-  printf("=> 추가됨!\n");
-  return 1;
-}
-
-void readRecruit(Recruit s){
-  printf("%s -> %s %04hd %04hd %hd/%hd %c\n", s.depart,s.arrive,s.date,s.time,s.now_num,s.num,s.type);
-}
-
-int updateRecruit(Recruit *s){
-  printf("\n출발지 입력 ");
-  scanf(" %[^\n]s", s->depart);
-  printf("도착지 입력 ");
-  scanf(" %[^\n]s", s->arrive );
-  printf("출발날짜 (예, 0326) ");
-  scanf("%hd", &s->date);
-  printf("출발시간 (예, 1940) ");
-  scanf("%hd", &s->time);
-  printf("모집인원 (2~4) ");
-  scanf("%hd", &s->num);
-  while(s->num < 2 || s->num > 4){
-    printf("모집 인원은 2명에서 4명까지 가능합니다.\n");
-    printf("모집인원 (2~4) ");
-    scanf(" %hd", &s->num);
-  }
-  printf("모집종류 (택시 : T, 카풀 : C) ");
-  scanf(" %c", &s->type);
-  printf("=> 수정됨!\n");
-  return 1;
-}
-
-int selectMenu(){
+int selectMenu(){ // 메뉴 선택
     int menu;
     printf("\n*** iTaxi ***\n");
     printf("1. 조회 ");
@@ -61,7 +12,7 @@ int selectMenu(){
     return menu;
 }
 
-void listRecruit(Recruit *s[], int count){
+void listRecruit(Recruit *s[], int count){ // 모집 리스트 출력
   printf("\nNo 출발지 -> 도착지 출발날짜 출발시간 모집현황 모집종류\n");
   printf("======================================================\n");
   for(int i=0; i<count; i++){
@@ -71,7 +22,7 @@ void listRecruit(Recruit *s[], int count){
   }
 }
 
-int selectDataNo(Recruit *s[], int count){
+int selectDataNo(Recruit *s[], int count){ // 데이터 번호 선택
   int no;
   listRecruit(s,count);
   printf("\n번호는 (취소 :0)? ");
@@ -79,7 +30,7 @@ int selectDataNo(Recruit *s[], int count){
   return no;
 }
 
-int joinRecruit(Recruit *s){
+int joinRecruit(Recruit *s){ // 모집 참여
   int joinok;
   if(s->now_num == s->num){
     printf("이미 마감된 모집입니다.\n");
@@ -94,7 +45,7 @@ int joinRecruit(Recruit *s){
   return 0;
 }
 
-int exitjoinRecruit(Recruit *s){
+int exitjoinRecruit(Recruit *s){ // 모집 참여 취소 
   int exitjoinok;
   if(s->now_num == 1){
     printf("해당 모집에 참여하지 않으셨습니다.\n");
