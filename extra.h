c0d3ff78cdc05d4a6c6717f307fd7e3512ce2a79
hanyoq15 <130302221+hanyoq15@users.noreply.github.com>
2023-05-14 00:18:05 +0900

    Update extra.h

--- extra.h
+++ extra.h
@@ -1,5 +1,16 @@
 #include <stdio.h>
-//#include "CRUD.h"
+
+typedef struct{
+	char depart[20]; // 출발지
+	char arrive[20]; // 도착지
+	short int date; // 출발날짜 (예, 0326)
+	short int time; // 출발시간 (예, 1940)
+	short int num; // 모집인원
+	char type; // 모집종류(T/C) (T: 택시, C: 카풀)
+	short int now_num; // 현재 모집된 인원
+	char memo[100]; // 메모
+	short int pw; // 비밀번호(4자리 숫자)
+} Recruit;
 
 int selectMenu(); // 메뉴 선택
 
