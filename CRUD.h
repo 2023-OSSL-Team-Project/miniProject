8283e1f6c214c42afb9a06ec121abe290f336a3d
hanyoq15 <130302221+hanyoq15@users.noreply.github.com>
2023-05-14 02:31:03 +0900

    Update CRUD.h

--- CRUD.h
+++ CRUD.h
@@ -30,8 +30,8 @@ void listRecruit(Recruit *s[], int count); // 모집 리스트 출력
 
 int joinRecruit(Recruit *s); // 모집 참여
 int exitjoinRecruit(Recruit *s); // 모집 참여 취소 
-int searchPlace(Recruit *s, int count); // 지점 검색
-int searchDay(Recruit *s, int count); // 날짜 검색
+int searchPlace(Recruit *s[], int count); // 지점 검색
+int searchDay(Recruit *s[], int count); // 날짜 검색
 
 int editTime(Recruit *s, int count); // 시간, 날짜 수정
 int editPlace(Recruit *s, int count); // 지점 수정
