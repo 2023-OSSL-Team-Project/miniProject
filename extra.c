e0af3cb8b072991eb21bedff909613e0a0e2f79b
seizethedaylyn <22000189@handong.ac.kr>
2023-05-21 02:37:32 +0900

    Update extra.c

--- extra.c
+++ extra.c
@@ -6,9 +6,9 @@ int selectMenu_main(){ // 메인화면 메뉴 선택
     printf("\n");
     printf("     +--------------+\n");
     printf("     |              |\n");
-    printf("  +---    ITAXI     ---+\n");
-    printf(" |                     |  == ** *\n");
-    printf(" ----------------------\n");
+    printf(" +---+    ITAXI     +---+\n");
+    printf(" |                      |  == ** *\n");
+    printf(" +----------------------+\n");
     printf("     ***         ***\n");
     printf("\n");
     printf("\n*** iTaxi Menu***\n");
@@ -16,6 +16,7 @@ int selectMenu_main(){ // 메인화면 메뉴 선택
     
     printf("1. 조회\n");
     printf("2. 모집\n");
+    printf("3. 저장\n");
     printf("0. 종료\n");
     printf("\n원하는 메뉴는? ");
     scanf("%d", &main0);
@@ -26,7 +27,7 @@ int selectMenu_main(){ // 메인화면 메뉴 선택
 int selectMenu1(){ // 조회 메뉴 선택
     int manu1;
     
-    printf("\n< 조회 >\n");
+    printf("\n***조회***\n");
     printf("1. 모집조회\n");
     printf("2. 모집참여\n");
     printf("3. 모집참여취소\n");
@@ -42,7 +43,7 @@ int selectMenu1(){ // 조회 메뉴 선택
 int selectMenu2(){ // 모집 메뉴 선택
     int manu2;
     
-    printf("\n< 모집 >\n");
+    printf("\n***모집***\n");
     printf("1. 모집글 작성\n");
     printf("2. 모집정보수정\n");
     printf("3. 모집삭제\n");
@@ -58,7 +59,7 @@ void listRecruit(Recruit *s, int index){ // 모집 리스트 출력
     printf("=============================\n");
     printf("\n");
     for(int i = 0; i < index; i++){
-        //if(s[i].date == -1) continue;
+        if(s[i].date == -1) continue;
         printf("NO.%2d\n", i+1);
         printf("\n");
         readRecruit(s[i]);
@@ -81,10 +82,10 @@ int selectDataNo(Recruit *s, int index){ // 데이터 번호 선택
   return no;
 }
 
-int joinRecruit(Recruit *s){ // 모집 참여
+int joinRecruit(Recruit *s, int index){ // 모집 참여
   int joinok;
 
-  if(s->now_num == s->num){
+  if((s+index)->now_num == (s+index)->num){
     printf("이미 마감된 모집입니다.\n");
     return 0;
   }
@@ -93,17 +94,17 @@ int joinRecruit(Recruit *s){ // 모집 참여
   scanf("%d",&joinok);
 
   if(joinok == 1){
-    s->now_num++;
+    (s+index)->now_num++;
     return 1;
   }
 
   return 0;
 }
 
-int exitjoinRecruit(Recruit *s){ // 모집 참여 취소 
+int exitjoinRecruit(Recruit *s, int index){ // 모집 참여 취소 
   int exitjoinok;
 
-  if(s->now_num == 1){
+  if((s+index)->now_num == 1){
     printf("해당 모집에 참여하지 않으셨습니다.\n");
     return 0;
   }
@@ -112,7 +113,7 @@ int exitjoinRecruit(Recruit *s){ // 모집 참여 취소
   scanf("%d",&exitjoinok);
 
   if(exitjoinok == 1){
-    s->now_num--;
+    (s+index)->now_num--;
     return 1;
   }
 
@@ -125,10 +126,10 @@ void searchPlace(Recruit *s, int index){
     char depart[20];
     char arrive[20];
 
-    printf("검색할 출발지 ");
+    printf("검색할 출발지: ");
     scanf("%s", depart);
 
-    printf("검색할 도착지 ");
+    printf("검색할 도착지: ");
     scanf("%s", arrive);
     
     printf("=========================\n");
@@ -157,7 +158,7 @@ void searchDay(Recruit *s, int count){
     int scnt = 0;
     short int search;
 
-    printf("검색할 날짜 (예, 0326) ");
+    printf("검색할 날짜 (예, 0522): ");
     scanf("%hd", &search);
     
     printf("=========================\n");
@@ -184,14 +185,17 @@ void searchDay(Recruit *s, int count){
 void updateMemo(Recruit *sp, int index){
     getchar();
 
-    char newmemo[20];
+    char newmemo[50];
 
     printf("new memo? ");
     fgets(newmemo, sizeof(newmemo), stdin);
     newmemo[strlen(newmemo)-1] = '\0';
 
     char newmemo_with_space[strlen(newmemo) + 1];
-    strcpy(newmemo_with_space, ", ");
+
+    if(strlen(sp[index].memo) != 0)
+      strcpy(newmemo_with_space, ", ");
+  
     strcat(newmemo_with_space, newmemo);
 
     strcat(sp[index].memo, newmemo_with_space);
