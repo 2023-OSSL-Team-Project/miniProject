9c1bc684813767e1660a9826c05923f7269942a2
seizethedaylyn <22000189@handong.ac.kr>
2023-05-21 02:36:54 +0900

    Update CRUD.c

--- CRUD.c
+++ CRUD.c
@@ -1,6 +1,7 @@
 #include "CRUD.h"
 
 int createRecruit(Recruit *s, int i){
+
   printf("\n출발지 입력: ");
 	scanf("%s", (s+i)->depart);
   
@@ -20,10 +21,10 @@ int createRecruit(Recruit *s, int i){
         else break;
   }
 
-  printf("모집종류 (택시 : T, 카풀 : C) ");
-  scanf(" %c", &s->type);
+  printf("모집종류 (택시 : T, 카풀 : C): ");
+  scanf(" %c", &(s+i)->type);
   
-  s->now_num = 1;
+  (s+i)->now_num = 1;
 
   printf("메모(없으면 엔터): ");
   getchar(); // 입력버퍼 지움
@@ -31,13 +32,14 @@ int createRecruit(Recruit *s, int i){
   (s+i) -> memo[strlen((s+i) -> memo) -1 ] = '\0';
 
   while(1){
-        printf("비밀번호 (4자리 숫자): ");
-        scanf("%hd", &(s+i)->pw);
-        while (getchar() != '\n'); // 버퍼 비움: 엔터값이 남아있으면 무한 루프 
-        if((s+i)->pw < 1000 || (s+i)->pw > 9999) printf("비밀번호는 4자리 숫자로 입력해주세요.\n");
-        else if(!((s+i)->pw)) printf("숫자로 입력해주세요.");
-        else break;
+    printf("비밀번호 (4자리 숫자): ");
+    scanf("%hd", &(s+i)->pw);
+    while (getchar() != '\n'); // 버퍼 비움: 엔터값이 남아있으면 무한 루프 
+    if((s+i)->pw < 1000 || (s+i)->pw > 9999) printf("비밀번호는 4자리 숫자로 입력해주세요.\n");
+    else if(!((s+i)->pw)) printf("숫자로 입력해주세요.");
+    else break;
   }
+
   return 1;
 }
 
@@ -73,8 +75,9 @@ int updateRecruit(Recruit *s, int i){
 
   printf("모집종류 (택시 : T, 카풀 : C): ");
   scanf(" %c", &(s+i)->type);
+  getchar();
 
-  s->now_num = 1;
+  (s+i)->now_num = 1;
 
   printf("메모(없으면 엔터 입력): ");
   getchar(); // 입력버퍼 지움
@@ -97,13 +100,62 @@ int deleteRecruit(Recruit *s, int i){
   short int password;
 
   while(1){
-        printf("비밀번호 (4자리 숫자): ");
-        scanf("%hd", &password );
-        while (getchar() != '\n'); // 버퍼 비움: 엔터값이 남아있으면 무한 루프 
-        if(s->pw != password) printf("비밀번호가 다릅니다.\n");
-        else break;
+    printf("비밀번호 (4자리 숫자): ");
+    scanf("%hd", &password );
+    while (getchar() != '\n'); // 버퍼 비움: 엔터값이 남아있으면 무한 루프 
+    if((s+i)->pw != password) printf("비밀번호가 다릅니다.\n");
+    else break;
   }
-  if(s+i) free(s+i);
 
+  (s+i)->date = -1;
   return 1;
+}
+
+void saveData(Recruit *sp, int index){
+    FILE *fp;
+    fp = fopen("itaxi.txt", "wt");
+
+    for(int i = 0; i < index; i++){
+        if(sp[i].date== -1) continue;
+        fprintf(fp,"\t%s %s %hd %hd %hd %c %hd %s\n", sp[i].depart, sp[i].arrive, sp[i].date, sp[i].time, sp[i].num, sp[i].type, sp[i].now_num, sp[i].memo);
+    }
+
+    fclose(fp);
+    printf("저장됨! \n");
+}
+
+int loadData(Recruit *sp){
+    int i = 0;
+
+    FILE *fp;
+    fp = fopen("itaxi.txt", "rt");
+
+    if(fp == NULL){
+        printf("=> 파일 없음!\n");
+    }
+    else{
+        for(; i < 20; i++){
+            fscanf(fp, "%s", (sp[i].depart));
+            fgetc(fp);
+            if(feof(fp)) break;
+            fscanf(fp, "%s", (sp[i].arrive));
+            fgetc(fp);
+            fscanf(fp,"%hd", &(sp[i].date));
+            fgetc(fp);
+            fscanf(fp, "%hd", &(sp[i].time));
+            fgetc(fp);
+            fscanf(fp, "%hd", &(sp[i].num));
+            fgetc(fp);
+            fscanf(fp, "%c", &(sp[i].type));
+            fgetc(fp);
+            fscanf(fp, "%hd", &(sp[i].now_num));
+            fgetc(fp);
+            fgets((sp+i) -> memo, sizeof((sp+i) -> memo), fp);
+            (sp + i) -> memo[strlen((sp + i) -> memo) - 1] = '\0';
+        }
+        fclose(fp);
+        printf("=> 로딩 성공!\n");
+    }
+
+    return i;
 }
