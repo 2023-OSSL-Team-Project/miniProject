d938f98a29c487c33085cb1cab44ec121d60c21c
rlacofls <100327184+seizethedaylyn@users.noreply.github.com>
2023-05-22 23:07:30 +0900

    Update extra.h

--- extra.h
+++ extra.h
@@ -16,8 +16,3 @@ int exitjoinRecruit(Recruit *s, int index); // 모집 참여 취소
 void searchPlace(Recruit *s, int index); // 지점 검색
 void searchDay(Recruit *s, int index); // 날짜 검색
 void updateMemo(Recruit *sp, int index);
-
-/*
-int editTime(Recruit *s, int count); // 시간, 날짜 수정
-int editPlace(Recruit *s, int count); // 지점 수정
-*/
